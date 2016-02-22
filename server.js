0de9db14c6297a321386bda2b9aabfca4b74e787
Chris Burgess <chris@giantrobot.co.nz>
2016-02-23 11:20:13 +1300

    Reformatting while debugging.

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ var express = require('express');
 var OAuth2 = require('./oauth2').OAuth2;
 var config = require('./config');
 
-
 // Express configuration
 var app = express();
 app.use(express.logger());
 app.use(express.bodyParser());
 app.use(express.cookieParser());
-app.use(express.session({
-    secret: "skjghskdjfhbqigohqdiouk"
-}));
+app.use(express.session({ secret: "SECRET" }));
 
 app.configure(function () {
     "use strict";
@@ -18,7 +15,6 @@ app.configure(function () {
     //app.use(express.logger());
     app.use(express.static(__dirname + '/public'));
 });
-
 
 // Config data from config.js file
 var client_id = config.client_id;
@@ -37,55 +33,46 @@ var oa = new OAuth2(client_id,
 
 // Handles requests to the main page
 app.get('/', function(req, res){
-
-    // If auth_token is not stored in a session cookie it sends a button to redirect to IDM authentication portal 
-    if(!req.session.access_token) {
-        res.send("Oauth2 IDM Demo.<br><br><button onclick='window.location.href=\"/auth\"'>Log in with FI-WARE Account</button>");
-
-    // If auth_token is stored in a session cookie it sends a button to get user info
-    } else {
-        res.send("Successfully authenticated. <br><br> Your oauth access_token: " +req.session.access_token + "<br><br><button onclick='window.location.href=\"/user_info\"'>Get my user info</button>");
-    }
+  // If auth_token is not stored in session it sends a button to redirect to authentication portal
+  if (!req.session.access_token) {
+    res.send("OAuth2 Demo.<br><br><button onclick='window.location.href=\"/auth\"'>Log in via OAuth2</button>");
+  // If auth_token is stored in a session cookie it sends a button to get user info
+  } else {
+    res.send("Successfully authenticated. <br><br> Your oauth access_token: " +req.session.access_token + "<br><br><button onclick='window.location.href=\"/user_info\"'>Get my user info</button>");
+  }
 });
 
-// Handles requests from IDM with the access code
+// Handles returning requests with the access code.
 app.get('/login', function(req, res){
-   
-    // Using the access code goes again to the IDM to obtain the access_token
-    oa.getOAuthAccessToken(req.query.code, function (e, results){
-
-        // Stores the access_token in a session cookie
-        req.session.access_token = results.access_token;
-
-        res.redirect('/');
-
-    });
+  // Using the access code goes again to the IDM to obtain the access_token
+  oa.getOAuthAccessToken(req.query.code, function (e, results) {
+    // Stores the access_token in a session cookie
+    req.session.access_token = results.access_token;
+    res.redirect('/');
+  });
 });
 
 // Redirection to IDM authentication portal
 app.get('/auth', function(req, res){
-    var path = oa.getAuthorizeUrl(response_type);
-    res.redirect(path);
+  var path = oa.getAuthorizeUrl(response_type);
+  res.redirect(path);
 });
 
 // Ask IDM for user info
 app.get('/user_info', function(req, res){
-    var url = config.idmURL + '/user/';
-
-    // Using the access token asks the IDM for the user info
-    oa.get(url, req.session.access_token, function (e, response) {
-
-        var user = JSON.parse(response);
-        res.send("Welcome " + user.displayName + "<br> Your email address is " + user.email + "<br><br><button onclick='window.location.href=\"/logout\"'>Log out</button>");
-    });
+  var url = config.idmURL + '/user/';
+  // Using the access token asks the IDM for the user info
+  oa.get(url, req.session.access_token, function (e, response) {
+    var user = JSON.parse(response);
+    res.send("Welcome " + user.displayName + "<br> Your email address is " + user.email + "<br><br><button onclick='window.location.href=\"/logout\"'>Log out</button>");
+  });
 });
 
 // Handles logout requests to remove access_token from the session cookie
 app.get('/logout', function(req, res){
-
-    req.session.access_token = undefined;
-    res.redirect('/');
+  req.session.access_token = undefined;
+  res.redirect('/');
 });
 
-console.log('Server listen in port 80. Connect to localhost');
-app.listen(80);
+console.log('Server listening, connect to http://localhost:' + config.server_port);
+app.listen(config.server_port);
